10dcc489fc52e01ca3561eaf9892996fbf09a4e5
Denis Pasha <111784197+DenisPasha@users.noreply.github.com>
2022-11-25 17:27:51 +0000

    Update

--- src/P01StachsAndQueues/lab/P01BrowserHistory.java
+++ src/P01StachsAndQueues/lab/P01BrowserHistory.java
@@ -8,37 +8,26 @@ public class P01BrowserHistory {
         Scanner scanner = new Scanner(System.in);
 
         String input=scanner.nextLine();
+        ArrayDeque<String>stack=new ArrayDeque<>();
 
-        ArrayDeque <String> stackURLs = new ArrayDeque<>();
-        ArrayDeque <String> forwardsQueue = new ArrayDeque<>();
 
         while (!input.equals("Home")){
+
             if (input.equals("back")){
-                if (stackURLs.size() < 2){
+                if (stack.size()<2){
                     System.out.println("no previous URLs");
+                }else {
+                    stack.pop();
+                    System.out.println(stack.peek());
                 }
-                else {
-                    String currentURL = stackURLs.pop();
-                    forwardsQueue.push(currentURL);
-                    System.out.println(stackURLs.peek());
-                }
-            }
-            else if (input.equals("forward")){
-                if (forwardsQueue.isEmpty()){
-                    System.out.println("no next URLs");
-                }
-                else {
-                    String currentForward = forwardsQueue.pop();
-                    System.out.println(currentForward);
-                    stackURLs.push(currentForward);
-                }
-            }
-            else {
-                stackURLs.push(input);
+            }else {
+                stack.push(input);
                 System.out.println(input);
-                forwardsQueue.clear();
             }
-            input = scanner.nextLine();
+
+
+            input=scanner.nextLine();
         }
+
     }
 }
