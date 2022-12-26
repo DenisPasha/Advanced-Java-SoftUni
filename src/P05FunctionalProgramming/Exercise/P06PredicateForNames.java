55b10c3f9e005fa8c1f2a6752826ea00d2cffa88
Denis Pasha <111784197+DenisPasha@users.noreply.github.com>
2022-12-26 19:17:46 +0000

    Update

---
+++ src/P05FunctionalProgramming/Exercise/P06PredicateForNames.java
@@ -0,0 +1,20 @@
+package P05FunctionalProgramming.Exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class P06PredicateForNames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        List<String> namesList = Arrays.stream(scanner.nextLine().split("\\s+")).collect(Collectors.toList());
+
+        Predicate<String>  predicate= name -> name.length() <=n;
+        namesList.stream().filter(predicate).forEach(person -> System.out.println(person));
+    }
+}
