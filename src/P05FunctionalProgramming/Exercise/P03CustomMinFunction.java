966a53dd7d35fcd64a5cd9504618632eb4cb3b5d
Denis Pasha <111784197+DenisPasha@users.noreply.github.com>
2022-12-26 13:42:25 +0000

    Update

--- src/P05FunctionalProgramming/Exercise/P03CustomMinFunction.java
+++ src/P05FunctionalProgramming/Exercise/P03CustomMinFunction.java
@@ -1,11 +1,19 @@
 package P05FunctionalProgramming.Exercise;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class P03CustomMinFunction {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        List<Integer>numsList = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
 
+        Function<List<Integer>,Integer> smallestNum = list -> Collections.min(list);
+        System.out.println(smallestNum.apply(numsList));
     }
 }
