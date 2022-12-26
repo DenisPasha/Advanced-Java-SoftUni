92cf77a1170143fc09ab030022d6cb7d891ab0f5
Denis Pasha <111784197+DenisPasha@users.noreply.github.com>
2022-12-26 19:26:38 +0000

    Update

---
+++ src/P05FunctionalProgramming/Exercise/P07FindTheSmallestElement.java
@@ -0,0 +1,21 @@
+package P05FunctionalProgramming.Exercise;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class P07FindTheSmallestElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> inputList = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+
+        Function<List<Integer>,Integer> smallestElementIndex = list -> list.lastIndexOf(Collections.min(list));
+       int smallest = smallestElementIndex.apply(inputList);
+        System.out.println(smallest);
+
+    }
+}
---
+++ src/P05FunctionalProgramming/Exercise/P08CustomComparator.java
@@ -0,0 +1,12 @@
+package P05FunctionalProgramming.Exercise;
+
+import java.util.Scanner;
+
+public class P08CustomComparator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
