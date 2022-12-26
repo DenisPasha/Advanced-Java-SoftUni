1e9430414278b6b51e6e90edb373a981a107e1fc
Denis Pasha <111784197+DenisPasha@users.noreply.github.com>
2022-12-26 19:01:23 +0000

    Update

--- src/P05FunctionalProgramming/Exercise/P05ReverseAndExclude.java
+++ src/P05FunctionalProgramming/Exercise/P05ReverseAndExclude.java
@@ -1,11 +1,26 @@
 package P05FunctionalProgramming.Exercise;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class P05ReverseAndExclude {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        List<Integer> inputList = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Collections.reverse(inputList);
+        Predicate<Integer> predicate = number -> number % n ==0;
+        inputList.removeIf(predicate);
+
+        inputList.forEach(number -> System.out.print(number+" "));
 
     }
 }
