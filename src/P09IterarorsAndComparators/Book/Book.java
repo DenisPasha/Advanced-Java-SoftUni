d0d8eee11eeb2166a2087d3df5b9ec7665c104b3
Denis Pasha <111784197+DenisPasha@users.noreply.github.com>
2023-01-05 20:17:50 +0000

    Update

--- src/P09IterarorsAndComparators/Book/Book.java
+++ src/P09IterarorsAndComparators/Book/Book.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Book {
+public class Book implements Comparable<Book> {
     private String title;
     private int year;
     private List<String> authors;
+
+    public int getYear() {
+        return year;
+    }
 
     public Book(String title, int year, String...authors) {
         this.title = title;
@@ -17,5 +21,15 @@ public class Book {
 
     public String getTitle() {
         return title;
+    }
+
+    @Override
+    public int compareTo(Book bookTwo) {
+        System.out.println();
+        int result =bookTwo.getTitle().compareTo(this.title);
+        if (result ==0 ){
+            result =  Integer.compare(this.year, bookTwo.getYear());
+        }
+        return result;
     }
 }
