1ada3d62105e2e54068f8b8e5447a1ffb26daa57
Denis Pasha <111784197+DenisPasha@users.noreply.github.com>
2023-01-10 14:47:33 +0000

    Update

--- src/P09IterarorsAndComparators/Exercise/ListIterator/ListiIterator.java
+++ src/P09IterarorsAndComparators/Exercise/ListIterator/ListiIterator.java
@@ -10,14 +10,6 @@ public class ListiIterator {
     }
 
 
-    //•	Move - should move an internal index position to the next index in the list, the method should return true
-    // if it successfully moved and false if there is no next index.
-
-    //•	HasNext - should return true if there is a next index and false if the index is already at the last element of the list.
-
-    //•	Print - should print the element at the current internal index,
-    // calling Print on a collection without elements should throw an appropriate exception with the message "Invalid Operation!".
-
     private int index = 0;
     private boolean isListEmpty() {
         if (this.list.size() > 0){
